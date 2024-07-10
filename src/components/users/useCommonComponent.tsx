fc0ec9b69e2a763e1cccfb8c5a75d98b90f77b20
narsipalle.mahesudu <narsipalle.mahesudu@happiestminds.com>
2024-07-10 20:53:15 +0530

    deleted unwanted components

--- src/components/users/useCommonComponent.tsx
+++ src/components/users/useCommonComponent.tsx
@@ -1,39 +1,15 @@
-import SearchIcon from '@mui/icons-material/Search'
-import { Button, InputAdornment, TextField } from '@mui/material'
-import { Dispatch, SetStateAction } from 'react'
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, InputAdornment, TextField } from "@mui/material";
 
-import { PROFILE_TYPE } from '@/components/enums/profileType.enum'
-import useApiRequest from '@/components/users/useApiRequest'
-
-const useCommonComponent = (
-  setRows: any,
-  setIsLoading: any,
-  setPage: Dispatch<SetStateAction<number>>,
-  setTotalRecords: Dispatch<SetStateAction<number>>,
-  profileType: string,
-  rowsPerPage: number,
-  searchValue: string,
-  setSearchValue: any,
-  roleIds: string,
-  sorting: string,
-) => {
-  const { getMethod } = useApiRequest()
-
+const useCommonComponent = () => {
   const searchComp = () => {
     const handleSearch = (e: any) => {
-      const specialChars = /[`!#$%^*()+\=\[\]{};:"\\|,<>\/?~\s]/
+      const specialChars = /[`!#$%^*()+\=\[\]{};:"\\|,<>\/?~\s]/;
       if (specialChars.test(e.target.value)) {
-        return
+        return;
       }
-      let target = e.target.value.replace(specialChars, '')
-
-      const url =
-        profileType === PROFILE_TYPE.USERS
-          ? `/user?offset=0&limit=${rowsPerPage}&searchText=${target}&roleids=${roleIds}${sorting}`
-          : `/jobseeker/job-seeker/list?offset=0&limit=${rowsPerPage}&searchText=${target}&${roleIds}${sorting}`
-      setPage(0)
-      getMethod(url, {}, setRows, setIsLoading, setTotalRecords)
-    }
+      let target = e.target.value.replace(specialChars, "");
+    };
 
     return (
       <>
@@ -41,60 +17,50 @@ const useCommonComponent = (
           sx={{
             mt: 2,
 
-            '& .MuiOutlinedInput-root': {
-              '& > fieldset': {
-                border: '1px solid #D4C4BE',
+            "& .MuiOutlinedInput-root": {
+              "& > fieldset": {
+                border: "1px solid #D4C4BE",
               },
             },
             input: {
-              '&::placeholder': {
-                fontSize: profileType !== PROFILE_TYPE.USERS ? '12px' : '16px',
+              "&::placeholder": {
+                fontSize: "16px",
               },
             },
           }}
           id="outlined-search"
           InputProps={{
-            [profileType == PROFILE_TYPE.USERS
-              ? 'startAdornment'
-              : 'endAdornment']: (
-              <InputAdornment
-                position={profileType == PROFILE_TYPE.USERS ? 'start' : 'end'}
-              >
+            startAdornment: (
+              <InputAdornment position="start">
                 <SearchIcon />
               </InputAdornment>
             ),
             style: {
-              border: '1px solid #DADAD9',
+              border: "1px solid #DADAD9",
               height: 40,
             },
           }}
-          placeholder={
-            profileType === PROFILE_TYPE.USERS
-              ? 'Search'
-              : 'Search by Name / Email / SSN'
-          }
+          placeholder="Search"
           type="search"
           onKeyDown={(e: any) => {
-            if (e.key === 'Enter') {
-              handleSearch(e)
+            if (e.key === "Enter") {
+              handleSearch(e);
             }
           }}
-          onChange={(e) => setSearchValue(e)}
         />
         <Button
-          onClick={() => handleSearch(searchValue)}
           variant="contained"
-          sx={{ mt: 2, ml: 1, minWidth: '40px', padding: '6px' }}
+          sx={{ mt: 2, ml: 1, minWidth: "40px", padding: "6px" }}
         >
           <SearchIcon />
         </Button>
       </>
-    )
-  }
+    );
+  };
 
   return {
     searchComp,
-  }
-}
+  };
+};
 
-export default useCommonComponent
+export default useCommonComponent;
